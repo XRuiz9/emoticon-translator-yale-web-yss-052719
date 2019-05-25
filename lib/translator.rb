8abd0472ca02a43e311507f55c4136a456c9a418
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-25 15:37:53 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,33 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file)
+  load = YAML.load_file(file)
+  emo = {}
+  emo["get_meaning"] = {}
+  emo["get_emoticon"] = {}
+  load.each do |emotion, emotes|
+    emo["get_emoticon"][emotes[0]] = emotes[1]
+    emo["get_meaning"][emotes[1]] = emotion
+  end
+  emo
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+  emo = load_library(file)
+  if emo["get_emoticon"][emoticon]
+    emo["get_emoticon"][emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+  emo = load_library(file)
+  if emo["get_meaning"][emoticon]
+    emo["get_meaning"][emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
